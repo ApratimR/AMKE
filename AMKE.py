35ec6ba7ddab6508da496b9d86fa78f42150a0f7
Apratim Ray <apratimr55@gmail.com>
2021-05-17 19:40:06 +0530

    Update AMKE.py
    Removed useless code

--- AMKE.py
+++ AMKE.py
@@ -5,13 +5,6 @@ import secrets
 charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
 
 
-#NOTE : make another variable for private rounds and use that for processing
-current_private_key = None        # =A     (Do not share)
-current_private_key_rounds = None # =A1    (Do not share)
-current_public_string = None      # =G     (sharable)
-current_public_key = None         # =AxG   ()
-current_shared_secret_key = None  # =AxGxB ()
-
 temp = 0
 # generates key for any use
 def generate_key():
@@ -19,7 +12,7 @@ def generate_key():
     return randombit
 
 def generate_rounds():
-    return secrets.randbelow(10)
+    return secrets.randbelow(10) #chage this to some other amount
 
 # string to 1D list[int]
 def string_to_int(inp):
@@ -60,45 +53,3 @@ def mixkey(public_key,private_key,private_rounds):
     return_key = np.reshape(return_key,64)
     return_key = int_to_string(return_key)
     return return_key
-
-
-def main_UI():
-    global current_private_key,current_public_string,current_public_key,current_shared_secret_key
-    print("""
-    Select your operation:
-    1.Generate yout own private key.
-    2.Set a previously generated shared secret key
-    3.create a public string and your public key with that public string for sending
-    (in: ->out: public_string public_key)
-    4.process the incoming public string and other users public key
-    (in:public_string senders_public_key ->out:your_public_key AND set: shared_secretkey)""")
-
-    option = int(input())
-
-    if option==1:
-
-        current_private_key = generate_key()
-
-
-    elif option==2:
-        current_shared_secret_key = str(input())
-        if current_shared_secret_key !=64:
-            current_shared_secret_key = None
-            raise Exception("Invalid KEY")
-
-    elif option==3:
-        if current_public_string==None:
-            current_public_string = generate_key()
-
-        if current_private_key == None:
-            current_private_key = generate_key()
-
-        #mixkey(current_public_string,current_private_key)
-
-
-
-
-
-if __name__ == "__main__":
-
-    main_UI()
